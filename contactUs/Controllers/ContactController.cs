871d3f5e12baa83218360074048be7a6858b97d0
Santiago Peñuñuri <penunurisantiago@gmail.com>
2020-11-18 02:51:24 -0700

    Se agrega varios cambios

---
+++ contactUs/Controllers/ContactController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using contactUs.Models;
+using System.Threading.Tasks;
+using contactUs.Repository;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace contactUs.Controllers 
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class ContactController : Controller
+    {
+
+        ContactRepository contactRepository = new ContactRepository();
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> add([FromBody] Contact contact)
+        {
+            var result = await contactRepository.add(contact);
+            
+
+            if (result == null)
+            {
+                return NotFound( new{message = "No se pudo crear el elemento deseado" });
+            }
+
+            return Ok(new
+            {
+                message = "Agregado",
+                data = result
+            });
+        }
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Contact get()
+        {
+            Contact contact = new Contact();
+            return contact;
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public List<Contact> getAll()
+        {
+            List<Contact> listContact = new List<Contact>();
+            return listContact;
+        }
+
+    }
+}
